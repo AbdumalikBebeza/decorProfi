1f59180450eb4b1525d3077b3daaabff867b615a
AbdumalikBebeza <sultan.bebeza@gmail.com>
2023-09-19 00:32:40 +0600

    add model Brand and Catalog

--- products/models.py
+++ products/models.py
@@ -2,10 +2,28 @@ from django.db import models
 from django_resized import ResizedImageField
 
 
+class Catalog(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     articul = models.CharField(max_length=30, verbose_name='Артикул')
     model = models.CharField(max_length=150, verbose_name='Модель')
+    catalog = models.ForeignKey(Catalog, on_delete=models.CASCADE, related_name='products',
+                                verbose_name='Категория продукта')
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products',
+                              verbose_name='Бренд продукта')
     description = models.TextField(verbose_name='Описание')
     color = models.CharField(max_length=50, verbose_name='Цвет')
     image1 = ResizedImageField(size=[200, 200], force_format='PNG', crop=['middle', 'center'], quality=75,
@@ -25,3 +43,11 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+    @property
+    def catalog_name(self):
+        return self.catalog.name
+
+    @property
+    def brand_name(self):
+        return self.brand.name
--- products/serializers.py
+++ products/serializers.py
@@ -3,6 +3,16 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    catalog_name = serializers.SerializerMethodField()
+    brand_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = "name articul model catalog brand description color image1 image2 image3" \
+                 " image4 information price catalog_name brand_name".split()
+
+    def get_catalog_name(self, product):
+        return product.catalog_name
+
+    def get_brand_name(self, product):
+        return product.brand_name
